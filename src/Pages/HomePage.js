cc041f9808082e4b9c7ee0ecde1ec7fb9015d1d9
Avaloqc <79610087+Avaloqc@users.noreply.github.com>
2023-07-02 13:17:16 -0400

    feat: :sparkles: Add functionality for saved pins Badge on Cards

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -7,16 +7,45 @@ import { Notification } from '../components/Notification/Notification';
 import { useAppContext } from '../storage/AppContext';
 import { SaveFolderSuccessType } from '../storage/types';
 
+const pinsData = [
+  {
+    id: '123',
+    title: 'Triginometria',
+    image: 'https://picsum.photos/200/300?53',
+    total: 0,
+  },
+  {
+    id: '234',
+    title: 'JavaScript',
+    image: 'https://picsum.photos/200/300?52',
+    total: 0,
+  },
+  {
+    id: '456',
+    title: 'Natureza',
+    image: 'https://picsum.photos/200/300?51',
+    total: 0,
+  }
+]
+
 export const HomePage = () => {
   const { state } = useAppContext();
-  const [ showAlert, setShowAlert ] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const pinsNormalized = pinsData.map(data => (
+    {
+      ...data,
+      total: state.folders.filter(folder => folder.pins.includes(data.id)).length
+    }
+  ))
+
 
   useEffect(() => {
     if (state.type === SaveFolderSuccessType) {
       setShowAlert(true);
     }
   }, [state.type])
-  
+
   const handleCloseAlert = () => {
     setShowAlert(false);
   }
@@ -32,15 +61,16 @@ export const HomePage = () => {
       <ModalCreateFolder show={state.mode === 'createFolder'} />
       <Container fluid>
         <Row>
-          <Col xs={12} md={2}>
-            <CardContainer 
-              id='123'
-              title="Título" 
-              image="https://picsum.photos/seed/picsum/200/235" 
-              total={3}
-              notif={handleCloseAlert} 
-            />
-          </Col>
+          {pinsNormalized.map(card => (
+            <Col key={card.id} xs={12} md={2}>
+              <CardContainer
+                id={card.id}
+                title={card.title}
+                image={card.image}
+                total={card.total}
+              />
+            </Col>
+          ))}
         </Row>
       </Container>
     </>
